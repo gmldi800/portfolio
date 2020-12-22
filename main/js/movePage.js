f30ae1303401bc35745bef5bb818c946be2edc51
gmldi800 <gmldi800@naver.com>
2020-12-22 15:10:24 +0900

    work

---
+++ main/js/movePage.js
@@ -0,0 +1,28 @@
+//$(function () {    
+//    console.log('aaaa')
+//    var next = $('.btns_container .next')
+//    console.log(next)
+//    var prev = $('.btns_container .prev')
+//
+//    var modal_01 = $('.modal_01')
+//    var modal_02 = $('.modal_02')
+//
+//
+//
+//    next.click(function () {
+//        console.log('aaaa')
+//        modal_02.show()
+//        modal_01.hide()
+//        prev.show()
+//        next.hide()
+//    })
+//
+//    prev.click(function () {
+//        console.log('bbbb')
+//        modal_02.hide()
+//        modal_01.show()
+//        next.show()
+//        prev.hide()
+//    })
+//
+//})
